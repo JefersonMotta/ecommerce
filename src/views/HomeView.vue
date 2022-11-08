ca33bad77e180d700570d6ecb2f856c0fd49060c
Jeferson Motta <jefersonmotta91@gmail.com>
2022-11-07 23:40:01 -0300

    atulizando dev

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+ <v-app>
+  <NavBar class="index"/>
+  <DashboardComp />
+  <FooterComp />
+ </v-app>
+</template>
+
+<script>
+import DashboardComp from '@/components/DashboardComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+import NavBar from '@/components/NavBar.vue';
+
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+    NavBar,
+    DashboardComp,
+    FooterComp
+},
+  
+        NavBar}
+</script>
+
+<style scoped>
+.index{
+  z-index: 9999;
+}
+</style>
